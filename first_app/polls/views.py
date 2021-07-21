4303ca92d07e8bf3275512d06502039cb8620a4f
Geo Thomas <geothomas@cet.ac.in>
2021-07-21 14:30:14 +0530

    sample

--- first_app/polls/views.py
+++ first_app/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Hello Welcome to Poll apps")
+    return render(request,'index.html')
+def login(request):
+    return render(request,'login.html')
 # Create your views here.
