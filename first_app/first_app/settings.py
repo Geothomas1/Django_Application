e47f21211d82b0798778ec4e55715aee15301a4c
Geo Thomas <geothomas@cet.ac.in>
2021-07-21 11:20:11 +0530

    sample

--- first_app/first_app/settings.py
+++ first_app/first_app/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--pjd8+(b-3&3cb)#$bwfsorg$efa0pd+gyf73r34g7l9jcp*&3'
-
+# SECRET_KEY = 'django-insecure--pjd8+(b-3&3cb)#$bwfsorg$efa0pd+gyf73r34g7l9jcp*&3'
+SECRET_KEY=''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
