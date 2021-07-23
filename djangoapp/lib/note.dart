63f59ee1005893c61fd3862f1740d0fd98997380
Geo Thomas <geothomas@cet.ac.in>
2021-07-23 15:13:30 +0530

    Flutter part

---
+++ djangoapp/lib/note.dart
@@ -0,0 +1,8 @@
+class Note{
+  int id;
+  String note;
+  Note({
+    required this.id,
+    required this.note
+  });
+}
--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'notes.urls'
@@ -125,3 +128,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS=True
